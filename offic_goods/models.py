2ba66f3e02937cfc8ad11050e05c945c54fa0e8f
pengboo <hz0126@163.com>
2019-04-03 17:02:23 +0800

    4.3

--- offic_goods/models.py
+++ offic_goods/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+#物品分类
+class TypeInfo(models.Model):
+    ttitle = models.CharField(max_length=20)
+    isDelete = models.BooleanField(default=False)
+
+
+class GoodsInfo(models.Model):
+    gtitle = models.CharField(max_length=20)
+    gpic = models.ImageField(upload_to='of_goods')
